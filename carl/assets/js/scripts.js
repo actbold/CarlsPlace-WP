fef9df0dca7b8ddba416c7b81aa0bf5729599b4c
Jose Luis <crasho1481@gmail.com>
2019-05-03 14:15:10 -0500

    I disabled the prev/next arrows on mobile and I enabled the dots to all devices

--- carl/assets/js/scripts.js
+++ carl/assets/js/scripts.js
@@ -51,41 +51,43 @@
 	
 	$(document).ready(function(){
 		 $('.gallery-slider').slick({
-		 dots: false,
-			 autoplay: false,
-			 arrows: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
+		    dots: true,
+			  autoplay: false,			  
+        infinite: true,
+        speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              arrows: true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              arrows: false, 
+              autoplay: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,                 
+              speed: 600,    
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,                
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
 		 }
 		 );
         }).on('setPosition', function (event, slick) {
